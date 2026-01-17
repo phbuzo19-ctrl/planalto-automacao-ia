c1df6d9ae0f77a363dfcf6c2fc75c8490957b912
Planalto Automação <phbuzo19@gmail.com>
2026-01-17 19:16:54 -0300

    Fix: construtor IndustrialHome compatível com rotas

--- lib/modules/industrial/industrial_home.dart
+++ lib/modules/industrial/industrial_home.dart
@@ -1,1 +1,17 @@
-import '../ia/ia_chat_page.dart';
+import 'package:flutter/material.dart';
+
+class IndustrialHome extends StatelessWidget {
+  const IndustrialHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Automação Industrial'),
+      ),
+      body: const Center(
+        child: Text('Módulo Industrial'),
+      ),
+    );
+  }
+}
